f35ec56bb3b597c3e4ede670fd205b376f83711d
Harsh <harshparecha@gmail.com>
2025-01-11 17:35:18 +0530

    setup database connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import 'dotenv/config';
+import { checkDatabaseConnection } from './db-connection';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -21,5 +22,6 @@ process.on('SIGINT', async () => {
 });
 
 app.listen(PORT, async () => {
+await checkDatabaseConnection();
   console.log(`Simple IAM running on port ${PORT}`);
 });
