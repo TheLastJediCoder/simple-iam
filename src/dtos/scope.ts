4396063b1455be3644535cb7cd613836723f24e0
TheLastJediCoder <harshparecha@gmail.com>
2025-01-13 19:28:41 +0530

    Added dtos for managing scopes

---
+++ src/dtos/scope.ts
@@ -0,0 +1,10 @@
+import { Scope, ScopeType } from "../models/scope";
+
+export interface CreateScopeRequest {
+  name: string;
+	type: ScopeType;
+}
+
+export type CreateScopeResponse = Scope;
+export type UpdateScopeRequest = Partial<CreateScopeRequest>;
+export type UpdateScopeResponse = Scope;
